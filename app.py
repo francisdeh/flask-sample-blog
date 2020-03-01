f5cce1711259a59e26243bf32d5fff667eea112a
= <=>
2020-03-01 15:46:20 +0000

    Implemented dummy post page and array of posts

--- app.py
+++ app.py
@@ -2,10 +2,28 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+# all posts array
+all_posts = [
+    {
+        'title': 'Post 1',
+        'content': 'This is the content of the post',
+        'author' : 'Jane'
+    },
+    {
+        'title': 'Post 2',
+        'content': 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Assumenda dolor doloremque velit voluptatibus. Ad, aut consequatur cumque, doloremque eius fugiat fugit iste iusto magnam minima nobis nulla officia perferendis repudiandae.'
+    }
+]
+
 
 @app.route('/')
 def index():
     return render_template('index.html')
+
+# Displays all posts
+@app.route('/posts')
+def posts():
+    return render_template('posts.html', posts=all_posts)
 
 
 @app.route('/home/<string:name>', methods=['GET'])
